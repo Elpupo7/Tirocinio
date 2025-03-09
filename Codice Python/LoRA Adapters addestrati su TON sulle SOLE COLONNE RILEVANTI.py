9e032a12af1d0b9024687e8c8a2b1f98610828a3
Niccolò <140309813+Elpupo7@users.noreply.github.com>
2025-03-09 21:23:51 +0100

    Update LoRA Adapters addestrati su TON sulle SOLE COLONNE RILEVANTI.py

--- Codice Python/LoRA Adapters addestrati su TON sulle SOLE COLONNE RILEVANTI.py
+++ Codice Python/LoRA Adapters addestrati su TON sulle SOLE COLONNE RILEVANTI.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from datasets import load_dataset, Dataset, DatasetDict
 from transformers import DistilBertTokenizer
-# CODICE PER ADDESTRARE I LOrA Adapter SUL DATASET TON CON LE COLE COLONNE RILEVANTI
+# CODICE PER ADDESTRARE I LORA Adapter SUL DATASET TON CON LE COLE COLONNE RILEVANTI
 df_full = pd.read_csv(windows10_dataset.csv)
 df_full = df_full.rename(columns={'label': 'Attack_label'})
 
@@ -48,25 +48,11 @@ df["type"] = df["type"].replace({
     "mitm": "MITM"
 })
 
-
 attack_dfs = {attack_type: df[df["type"] == attack_type]
               for attack_type in df["type"].unique()}
 
-attack_samples = {
-    attack_type: (
-         attack_dfs["SQL_injection"] if attack_type == "SQL_injection"
-        else attack_dfs["MITM"] if attack_type == "MITM"
-        else attack_dfs["DDoS_TCP"] if attack_type == "DDoS_TCP"
-        else attack_dfs["Port_Scanning"] if attack_type == "Port_Scanning"
-        else attack_dfs["XSS"] if attack_type == "XSS"
-        else attack_dfs["Password"] if attack_type == "Password"
-        else attack_dfs["DDoS_UDP"] if attack_type == "DDoS_UDP"
-        else df.sample(n=5000, random_state=42) 
-    )                                          
-    for attack_type, df in attack_dfs.items()
-}
 
-df1 = pd.concat(list(attack_samples.values())).sample(frac=1, random_state=42)
+df1 = pd.concat(list(attack_dfs.values())).sample(frac=1, random_state=42)
 print("Dimensione del nuovo DataFrame:", df1.shape)
 print("Distribuzione di Attack_label:")
 print(df1["Attack_label"].value_counts())
