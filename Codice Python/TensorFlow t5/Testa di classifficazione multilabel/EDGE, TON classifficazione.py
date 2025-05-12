420914f8856854c1781dba16938487786ca10d49
Niccolò <140309813+Elpupo7@users.noreply.github.com>
2025-05-12 19:12:08 +0200

    Update EDGE, TON classifficazione.py

--- Codice Python/TensorFlow t5/Testa di classifficazione multilabel/EDGE, TON classifficazione.py
+++ Codice Python/TensorFlow t5/Testa di classifficazione multilabel/EDGE, TON classifficazione.py
@@ -1,1 +1,31 @@
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+from transformers import T5Tokenizer, TFT5ForConditionalGeneration, T5Config
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_fscore_support
+from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
+
+DATASET = 'EdgeIoT_250_rows.csv'
+#DATASET = 'TON_350_rows.csv'
+LABEL_COLUMNS = ["Attack_type","Attack_label"]  # List of target columns
+EPOCHS = 3
+batch_size = 32
+MAX_LENGTH = 128
+MODEL_NAME = 'google/t5-efficient-tiny'  # Use a valid T5 model
+OUTPUT_MODEL = f'models/t5_trained.pth'
+
+# Load dataset
+print(f"[+] Loading dataset {DATASET}...")
+df1 = pd.read_csv(DATASET, delimiter=",")
+
+
+
+
+
+
 
